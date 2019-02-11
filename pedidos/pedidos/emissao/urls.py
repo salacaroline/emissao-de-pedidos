bbe5ddf27d1c1099bb6b102e49fc3279ccdaa0f7
CAROLINE <carolinesala16@gmail.com>
2019-02-11 18:39:41 -0200

    Inicio dos templates para cadastro de pedidos

--- pedidos/pedidos/emissao/urls.py
+++ pedidos/pedidos/emissao/urls.py
@@ -5,4 +5,6 @@ from . import views
 #Criando rotas html
 urlpatterns = [
      path('', views.index, name='index'),
+     path('cadastrar-pedido/', views.cadastro, name = 'cadastro'),
+     path('cadastrar-pedido/selecionar-produtos/', views.selecionar_produtos, name = 'selecionar_produtos'),
 ]
--- pedidos/pedidos/emissao/views.py
+++ pedidos/pedidos/emissao/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+from .models import Clientes
 # Create your views here.
 def index(request):
     return render(request, 'emissao/index.html', {})
+
+def cadastro(request):
+	clientes = Clientes.objects.all()
+    return render(request, 'emissao/cadastrar-pedidos.html', {})
+
+def selecionar_produtos(request):
+    return render(request, 'emissao/selecionar-produtos.html', {})
